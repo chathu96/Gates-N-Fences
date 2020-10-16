4e96e8762d243b254daa96a42d859d7b77657151
Chathu Aththanayaka <chathuryaatthanayake2015@gmail.com>
2020-10-16 15:53:59 +0530

    insert contacts

--- script1.js
+++ script1.js
@@ -11,18 +11,6 @@ function onFormSubmit() {
     }
 }
 
-function readFormData() {
-    var formData = {};
-    formData["fullName"] = document.getElementById("fullName").value;
-    formData["address"] = document.getElementById("address").value;
-    formData["city"] = document.getElementById("city").value;
-    formData["state"] = document.getElementById("state").value;
-    formData["zipcode"] = document.getElementById("zipcode").value;
-    formData["email"] = document.getElementById("email").value;
-    formData["quotation"] = document.getElementById("quotation").value;
-    return formData;
-}
-
 function insertNewRecord(data) {
     var table = document.getElementById("contactList").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
@@ -54,44 +42,4 @@ function resetForm() {
     document.getElementById("email").value = "";
     document.getElementById("quotation").value = "";
     selectedRow = null;
-}
-
-function onEdit(td) {
-    selectedRow = td.parentElement.parentElement;
-    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("address").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("city").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("state").value = selectedRow.cells[3].innerHTML;
-    document.getElementById("zipcode").value = selectedRow.cells[4].innerHTML;
-    document.getElementById("email").value = selectedRow.cells[5].innerHTML;
-    document.getElementById("quotation").value = selectedRow.cells[6].innerHTML;
-}
-function updateRecord(formData) {
-    selectedRow.cells[0].innerHTML = formData.fullName;
-    selectedRow.cells[1].innerHTML = formData.address;
-    selectedRow.cells[2].innerHTML = formData.city;
-    selectedRow.cells[3].innerHTML = formData.state;
-    selectedRow.cells[4].innerHTML = formData.zipcode;
-    selectedRow.cells[5].innerHTML = formData.email;
-    selectedRow.cells[6].innerHTML = formData.quotation;
-}
-
-function onDelete(td) {
-    if (confirm('Are you sure to delete this record ?')) {
-        row = td.parentElement.parentElement;
-        document.getElementById("contactList").deleteRow(row.rowIndex);
-        resetForm();
-    }
-}zipcode
-function validate() {
-    isValid = true;
-    if (document.getElementById("fullName").value == "") {
-        isValid = false;
-        document.getElementById("fullNameValidationError").classList.remove("hide");
-    } else {
-        isValid = true;
-        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
-            document.getElementById("fullNameValidationError").classList.add("hide");
-    }
-    return isValid;
 }
