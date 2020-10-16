3247680f1a34eef55e4c7fe8a9883ddf3e4a9039
Chathu Aththanayaka <chathuryaatthanayake2015@gmail.com>
2020-10-16 13:20:30 +0530

    Adding js for text fields of Contact Us

--- script1.js
+++ script1.js
@@ -14,9 +14,12 @@ function onFormSubmit() {
 function readFormData() {
     var formData = {};
     formData["fullName"] = document.getElementById("fullName").value;
-    formData["empCode"] = document.getElementById("empCode").value;
-    formData["salary"] = document.getElementById("salary").value;
+    formData["address"] = document.getElementById("address").value;
     formData["city"] = document.getElementById("city").value;
+    formData["state"] = document.getElementById("state").value;
+    formData["zipcode"] = document.getElementById("zipcode").value;
+    formData["email"] = document.getElementById("email").value;
+    formData["quotation"] = document.getElementById("quotation").value;
     return formData;
 }
 
@@ -26,13 +29,19 @@ function insertNewRecord(data) {
     cell1 = newRow.insertCell(0);
     cell1.innerHTML = data.fullName;
     cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.empCode;
+    cell2.innerHTML = data.address;
     cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.salary;
+    cell3.innerHTML = data.city;
     cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.city;
-    cell4 = newRow.insertCell(4);
-    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+    cell4.innerHTML = data.state;
+    cell5 = newRow.insertCell(4);
+    cell5.innerHTML = data.zipcode;
+    cell6 = newRow.insertCell(5);
+    cell6.innerHTML = data.email;
+    cell4 = newRow.insertCell(6);
+    cell4.innerHTML = data.quotation;
+    cell4 = newRow.insertCell(7);
+    cell4.innerHTML = `<a onClick="onEdit(this)" >Edit</a>
                        <a onClick="onDelete(this)">Delete</a>`;
 }
 
@@ -64,7 +73,7 @@ function onDelete(td) {
         document.getElementById("employeeList").deleteRow(row.rowIndex);
         resetForm();
     }
-}
+}zipcode
 function validate() {
     isValid = true;
     if (document.getElementById("fullName").value == "") {
